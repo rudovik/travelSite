ffb59e5540966df8e8cc160bb475f3f3ebe4acec
rudovik <giguni@ukr.net>
2020-11-28 11:53:58 +0200

    Completed lazy loading of images.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll'
 
 class StickyHeader {
   constructor() {
+    this.lazyImages = $('.lazyload')
     this.siteHeader = $('.site-header')
     this.headerTriggerElement = $('.large-hero__title')
     this.pageSections = $('.page-section')
@@ -12,6 +13,11 @@ class StickyHeader {
     this.createHeaderWaypoint()
     this.createPageSectionWaypoints()
     this.addSmoothScrolling()
+    this.refreshWaypoints()
+  }
+
+  refreshWaypoints() {
+    this.lazyImages.on('load', Waypoint.refreshAll)
   }
 
   addSmoothScrolling() {
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ import path from 'path'
 
 export default {
   mode: 'development',
-  entry: './app/assets/scripts/App.js',
+  entry: {
+    App: './app/assets/scripts/App.js',
+    Vendor: './app/assets/scripts/Vendor.js',
+  },
   output: {
     path: path.resolve(path.resolve(), 'app/temp/scripts'),
-    filename: 'App.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
