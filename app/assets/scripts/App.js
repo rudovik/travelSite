db1a9ace7d274fcca42d3142da754fad85816c3d
rudovik <giguni@ukr.net>
2020-11-28 10:18:36 +0200

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,10 +2,12 @@ import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
 import $ from 'jquery'
 import StickyHeader from './modules/StickyHeader.js'
+import Modal from './modules/Modal.js'
 
 window.onload = () => {
   new MobileMenu()
   new RevealOnScroll($('.feature-item'), '85%')
   new RevealOnScroll($('.testimonial'), '60%')
   new StickyHeader()
+  new Modal()
 }
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,36 @@
+import $ from 'jquery'
+
+class Modal {
+  constructor() {
+    this.openModalButton = $('.open-modal')
+    this.modal = $('.modal')
+    this.closeModalButton = $('.modal__close')
+    this.events()
+  }
+
+  events() {
+    // clicking the open modal button
+    this.openModalButton.on('click', this.openModal.bind(this))
+    // clicking the x close modal button
+    this.closeModalButton.on('click', this.closeModal.bind(this))
+    // pushes the escape key
+    $(document).on('keyup', this.keyPressHandler.bind(this))
+  }
+
+  keyPressHandler(e) {
+    if (e.keyCode == 27) {
+      this.closeModal()
+    }
+  }
+
+  openModal() {
+    this.modal.addClass('modal--is-visible')
+    return false
+  }
+
+  closeModal() {
+    this.modal.removeClass('modal--is-visible')
+  }
+}
+
+export default Modal
